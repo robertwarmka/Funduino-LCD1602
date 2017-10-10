f958fdfe93dfeeb408b2cd0b8f8a050d36708e00
Robert Warmka <robertwarmka@gmail.com>
2017-10-09 22:28:46 -0500

    Initial Repo Commit

---
+++ LCD1602.ino
@@ -0,0 +1,44 @@
+//LCD1602
+//You should now see your LCD1602 display the flowing characters "SUNFOUNDER" and "hello, world"
+//Email:support@sunfounder.com
+//Website:www.sunfounder.com
+//2015.5.7 
+#include <LiquidCrystal.h>// include the library code
+/**********************************************************/
+char array1[]="Hello, world!";
+char array2[]="From: ATmega328";  //the string to print on the LCD
+int delayTime = 50; // ms
+int blinkTime = 3000; // ms
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
+/*********************************************************/
+void setup() {
+  lcd.begin(16, 2);  // set up the LCD's number of columns and rows:
+  lcd.setCursor(0, 0);
+  lcd.print("Woo!");
+}
+/*********************************************************/
+void loop() {
+    lcd.clear();
+    lcd.home();
+    
+    for(int posCounter1 = 0; posCounter1 < sizeof(array1) - 1; ++posCounter1) {
+        lcd.print(array1[posCounter1]);
+        delay(delayTime);
+    }
+    
+    lcd.blink();
+    delay(blinkTime);
+    lcd.noBlink();
+    
+    lcd.setCursor(0, 1);
+    for(int posCounter2 = 0; posCounter2 < sizeof(array2) - 1; ++posCounter2) {
+        lcd.print(array2[posCounter2]);
+        delay(delayTime);
+    }
+    
+    lcd.blink();
+    delay(blinkTime);
+    lcd.noBlink();
+}
+/************************************************************/
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Funduino LCD1602
+A starting point for my development of different applications for my Arduino Pro Mini knock-off.
